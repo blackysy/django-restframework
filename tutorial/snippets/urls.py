78291dfbc6b6136d92a1f3d300766fc1d2d9f85f
yangh <yang.hong@sand.com.cn>
2019-12-05 10:17:43 +0800

    请求和响应

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -6,6 +6,7 @@
 # @Software : PyCharm
 
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from .views import *
 
 
@@ -13,3 +14,5 @@ urlpatterns = [
     url(r'^snippets/$', snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
